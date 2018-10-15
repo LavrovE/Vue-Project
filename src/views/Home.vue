a3570426cd01966031b887025307eea307f812a6
Eugene <1lavrov1@ukr.net>
2018-10-15 20:32:39 +0300

    vuetify libb with polyfill installed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="image" class="image" src="../assets/image.jpg">
-    <FirstComponent msg="Проверяем работу polyfill, babel, vuetify"/>
-  </div>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <blockquote>
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import FirstComponent from '@/components/FirstComponent.vue'
-
-export default {
-  name: 'home',
-  components: {
-      FirstComponent
-  }
-}
-</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .image{
-  max-width:400px;
-  height:auto;
-  }
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
